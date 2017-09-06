af06ed14ed766a6d5f3098a500effccd9cd494e3
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-06 14:43:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,6 @@ function addElementToEndOfArray(array, element){
   return array.push(element);
 }
 
-function destructivelyAddElementToEndOfArray {
+function destructivelyAddElementToEndOfArray() {
 
 }
