f372f30df6adcc8bb4299656bcba9828902a171f
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-06 14:30:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,12 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return array;
+  [beginning, ...array];
+  
 }
 
 function destructivelyAddElementToBeginningOfArray(array, beginning) {
-  [beginning, ...array];
+  array.unshift(element);
   return array;
 }
 console.log (addElementToBeginningOfArray(chocolateBars, "hello"))
