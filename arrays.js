7270dd15e3eb03967e69ac949c02af5f919ddbff
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-06 15:18:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,8 +11,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element){
-  return array = array.push(element);
-
+  array.push(element);
+  return array;
 }
 
 function destructivelyAddElementToEndOfArray() {
